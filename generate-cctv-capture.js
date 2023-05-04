adc7010315de9db0a92b8dba6838bb9800b3c00a
Krisanathep Boonpromma <krisanathep.b@gli.co.th>
2023-05-04 10:07:52 +0700

    Change serial to uuid

--- generate-cctv-capture.js
+++ generate-cctv-capture.js
@@ -1,24 +1,33 @@
-const Jimp = require('jimp') ;
+const Jimp = require('jimp');
 const moment = require('moment');
 
-async function textOverlay(srcImage, destImage, text='') {
-   // Reading image
-   const image = await Jimp.read(`./images/${srcImage}`);
-   // Defining the text font
-   const font = await Jimp.loadFont(Jimp.FONT_SANS_32_WHITE);
-   image.print(font, 380, 470, text);
-   // Writing image after processing
-   await image.writeAsync(`./out/${destImage}`);
+const TiME_STAMP_POSITON_COORD = {
+  x: 16,
+  y: 16,
+};
+
+async function textOverlay(srcImage, destImage, text = '') {
+  // Reading image
+  const image = await Jimp.read(`./images/${srcImage}`);
+  // Defining the text font
+  const font = await Jimp.loadFont(Jimp.FONT_SANS_32_WHITE);
+  image.print(
+    font,
+    TiME_STAMP_POSITON_COORD.x,
+    TiME_STAMP_POSITON_COORD.y,
+    text
+  );
+  // Writing image after processing
+  await image.writeAsync(`./out/${destImage}`);
 }
 
-for (let i = 0; i < 24; i++ ) {
-  const stationCode = 'simulator';
-  const serial = '000000';
-  const time = moment().startOf('D').add(i, 'h')
+for (let i = 0; i < 24; i++) {
+  const streamId = '8e44da44-6605-4d66-8d3e-ca435cb74ce7';
+  const time = moment().startOf('D').add(i, 'h');
   const timestamp = time.format('YYYYMMDDHHmmSSSSSS');
-  const text = time.format('HH:mm:ss');
-  const event = 'TIMING';
-  const filename = `${stationCode}_${serial}_${timestamp}_${event}`;
-  textOverlay('raw-image.png', `${filename}.png`, text);
+  const text = time.format('DD MMM YYYY HH:mm:ss');
+  const event = 'API';
+  const filename = `${streamId}_${timestamp}_${event}`;
+  textOverlay('กลางวัน.png', `${filename}.png`, text);
 }
-console.log("Image is processed succesfully");
+console.log('Image is processed succesfully');
